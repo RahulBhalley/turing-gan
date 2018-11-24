b513b5ee0bbf91b81d0058469c65487594f470d7
Rahul Bhalley <rahulbhalley@Rahuls-MacBook-Pro.local>
2018-11-24 14:03:43 +0530

    commit

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
-from __future__ import division
 
 __author__ = "Rahul Bhalley"
 
@@ -12,18 +11,16 @@ if IMG_DIM == 32:
 elif IMG_DIM == 256:
     Z_DIM = 100
 BATCH_SIZE = 64
-MODE = 'wgan' # [wgan, sgan]
+MODE = 'sgan' # [wgan, sgan]
 
 # Some other configurations
-DATASET = 'cifar-100' # [mnist, fashion-mnist, cifar-10, cifar-100, ...]
-if DATASET in ['fashion-mnist', 'mnist']:
-    N_CHANNELS = 1
-else:
-    N_CHANNELS = 3
+DATASET = 'fashion-mnist' # [mnist, fashion-mnist, cifar-10, cifar-100, ...]
+N_CHANNELS = 1 if DATASET in ['fashion-mnist', 'mnist'] else 3
 BEGIN_ITER = 0
-TOTAL_ITERS = 1000000
+TOTAL_ITERS = 100000
 ITERS_PER_LOG = 100
-VERBOSE = True
+VERBOSE = False
+TRAIN = False
 
 print('------------------')
 print('| Configurations |')
